5568b8ec25d01c2484839543dc04cbea17f6411a
darynabarysiuk <darinka.borisyuk.2002@gmail.com>
2021-12-04 01:11:12 +0300

    Implement basic repository and  Entity framework

---
+++ ExchangeOffice.IRepositories/IBaseRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq.Expressions;
+
+namespace ExchangeOffice.IRepositories
+{
+    public interface IBaseRepository<TEntity>
+    {
+        Task<TEntity> CreateAsync(TEntity entity);
+
+        Task<IEnumerable<TEntity>> CreateRangeAsync(IEnumerable<TEntity> entities);
+
+        Task<TEntity> UpdateAsync(TEntity entity);
+
+        Task UpdateRangeAsync(IEnumerable<TEntity> entities);
+
+        Task RemoveAsync(TEntity entity);
+
+        Task<int> RemoveRangeAsync(IEnumerable<TEntity> entities);
+
+        Task<TEntity> GetByAsync(Expression<Func<TEntity, bool>> expression, params Expression<Func<TEntity, object>>[] includes);
+
+        IQueryable<TEntity> GetAllQueryable();
+
+        IQueryable<TEntity> GetAllByQueryable(Expression<Func<TEntity, bool>> expression, params Expression<Func<TEntity, object>>[] includes);
+    }
+}
---
+++ ExchangeOffice.Repositories/BaseRepository.cs
@@ -0,0 +1,84 @@
+﻿using ExchangeOffice.IRepositories;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ExchangeOffice.Repositories
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity>
+        where TEntity : class
+    {
+        protected readonly ApplicationContext _context;
+
+        protected readonly DbSet<TEntity> _dbSet;
+
+        public BaseRepository(ApplicationContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public async Task<TEntity> CreateAsync(TEntity entity)
+        {
+            var newEntity = await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return newEntity.Entity;
+        }
+
+        public async Task<IEnumerable<TEntity>> CreateRangeAsync(IEnumerable<TEntity> entities)
+        {
+            _dbSet.AddRange(entities);
+            await _context.SaveChangesAsync();
+            return entities;
+        }
+
+        public IQueryable<TEntity> GetAllByQueryable(Expression<Func<TEntity, bool>> expression, params Expression<Func<TEntity, object>>[] includes)
+        {
+            IQueryable<TEntity> query = _dbSet;
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+            return query.Where(expression);
+        }
+
+        public IQueryable<TEntity> GetAllQueryable()
+        {
+            return _dbSet.AsQueryable();
+        }
+
+        public async Task<TEntity> GetByAsync(Expression<Func<TEntity, bool>> expression, params Expression<Func<TEntity, object>>[] includes)
+        {
+            IQueryable<TEntity> query = _dbSet;
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+            return await query.FirstOrDefaultAsync(expression);
+        }
+
+        public async Task RemoveAsync(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> RemoveRangeAsync(IEnumerable<TEntity> entities)
+        {
+            _dbSet.RemoveRange(entities);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task UpdateRangeAsync(IEnumerable<TEntity> entities)
+        {
+            _dbSet.UpdateRange(entities);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
