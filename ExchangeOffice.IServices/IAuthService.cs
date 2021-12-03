b2f8c88c2167c96ccd63f07917161881fec0b906
darynabarysiuk <darinka.borisyuk.2002@gmail.com>
2021-12-04 02:56:18 +0300

    Add auth service

---
+++ ExchangeOffice.IServices/IAuthService.cs
@@ -0,0 +1,7 @@
+﻿namespace ExchangeOffice.IServices
+{
+    public interface IAuthService
+    {
+        public Task<bool> Authentificate(string login, string password);
+    }
+}
---
+++ ExchangeOffice.Services/AuthService.cs
@@ -0,0 +1,30 @@
+﻿using ExchangeOffice.IRepositories;
+using ExchangeOffice.IServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ExchangeOffice.Services
+{
+    public class AuthService : IAuthService
+    {
+        IRepositoryCashers repositoryCashers;
+
+        public AuthService(IRepositoryCashers repositoryCashers)
+        {
+            this.repositoryCashers = repositoryCashers;
+        }
+
+        public async Task<bool> Authentificate(string login, string password)
+        {
+            string hash;
+            using (var sha256 = SHA256.Create())
+            {
+                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                hash = BitConverter.ToString(hashedBytes);
+            }
+            var casher = await repositoryCashers.GetByAsync(c => c.Login == login && c.PasswordHash == hash);
+
+            return casher != null;
+        }
+    }
+}
